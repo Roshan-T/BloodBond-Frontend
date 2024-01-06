33d819543a0ae6de9e7aa52278183dde66e7860f
Roshan-T <roshantiwari9827@gmail.com>
2024-01-06 23:29:04 +0545

    home screen simple modify

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -16,53 +16,88 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Container(
-          padding: const EdgeInsets.all(15),
-          child: Column(
-            children: [
-              Row(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: CircleAvatar(
-                      radius: 35,
-                      child: Image.asset("assets/images/onboarding2.png"),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 15),
+          child: SingleChildScrollView(
+            physics: const ClampingScrollPhysics(),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: CircleAvatar(
+                        radius: 35,
+                        child: Image.asset("assets/images/onboarding2.png"),
+                      ),
                     ),
-                  ),
-                  const SizedBox(
-                    width: 15,
-                  ),
-                  Column(
-                    children: [
-                      Text(
-                        "Hi Roshan,",
-                        style: Get.textTheme.titleLarge?.copyWith(
-                            color: Constants.kBlackColor,
-                            fontWeight: FontWeight.bold),
-                      ),
-                      const SizedBox(
-                        height: 8,
-                      ),
-                      Text(
-                        "Welcome Back",
-                        style: Get.textTheme.labelMedium?.copyWith(
-                          color: Constants.kBlackColor,
+                    const SizedBox(
+                      width: 15,
+                    ),
+                    Column(
+                      children: [
+                        Text(
+                          "Hi Roshan,",
+                          style: Get.textTheme.titleLarge?.copyWith(
+                              color: Constants.kBlackColor,
+                              fontWeight: FontWeight.bold),
                         ),
-                      )
-                    ],
-                  ),
-                  const Spacer(),
-                  const IconButton(
-                    onPressed: null,
-                    icon: Icon(
-                      Icons.notifications_active_rounded,
-                      color: Constants.kPrimaryColor,
-                      size: 30,
+                        const SizedBox(
+                          height: 8,
+                        ),
+                        Text(
+                          "Welcome Back",
+                          style: Get.textTheme.labelMedium?.copyWith(
+                            color: Constants.kBlackColor,
+                          ),
+                        )
+                      ],
                     ),
-                  )
-                ],
-              )
-            ],
+                    const Spacer(),
+                    const IconButton(
+                      onPressed: null,
+                      icon: Icon(
+                        Icons.notifications_active_rounded,
+                        color: Constants.kPrimaryColor,
+                        size: 30,
+                      ),
+                    )
+                  ],
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                Text(
+                  "Upcoming bootcamp",
+                  style: Get.textTheme.displayLarge
+                      ?.copyWith(color: Constants.kBlackColor, fontSize: 20),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                Container(
+                  height: 300,
+                  child: ListView.separated(
+                      physics: const ClampingScrollPhysics(),
+                      itemBuilder: (context, index) {
+                        return Container(
+                          height: 300,
+                          width: 350,
+                          color: Colors.amber,
+                        );
+                      },
+                      separatorBuilder: (context, index) {
+                        return const SizedBox(
+                          width: 10,
+                        );
+                      },
+                      shrinkWrap: true,
+                      scrollDirection: Axis.horizontal,
+                      itemCount: 5),
+                )
+              ],
+            ),
           ),
         ),
       ),
